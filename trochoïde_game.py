d5aa77341722b65fb611caa2edd68e0a3cee617f
Clément Thion <clement.thion@orange.fr>
2020-04-20 17:38:31 +0200

    added movator fonction in trochoide_game.py

--- trochoïde_game.py
+++ trochoïde_game.py
@@ -8,6 +8,44 @@ __date__    = "2018-01-15"
 from ezTK import *
 from math import *
 # ==============================================================================
+
+
+
+def movator():
+    """vitesse linéaire: d/t : (d=2px)/(t=10ms)
+    ==> vitesse constante pour l'instant, on verra plus tard
+    pour passer à v en dérivé."""
+
+    xPw, yPw, xQw, yQw = win.canvas.coords(win.roulette)
+    xC, yC = (xPw+xQw)/2,(yPw+yQw)/2 #coords du centre des cercles
+    xP, yP, xQ, yQ = xPw-xC, yPw-yC, xQw-xC, yQw-yC #changement de variabe, on se ramène à l'origine du repère
+    R = sqrt((xQ-xP)**2+(yQ-yP)**2)/2 #rayon du cercle contenant le carré contenant la roulette.
+    #a = d/(2*R) #angle de rotation, dépendant de d
+    
+    for k in range(1,4):
+        a = radians(k*20)
+    #nouvelles coords du point P
+        xP2 = xP*cos(a)+yP*sin(a) + xC 
+        yP2 = -xP*sin(a)+yP*cos(a) + yC 
+    #nouvelles coords du point Q
+        xQ2 = xQ*cos(a)+yQ*sin(a) + xC 
+        yQ2 = -xQ*sin(a)+yQ*cos(a) + yC
+
+        #win.canvas.create_oval(xP2, yP2, xQ2, yQ2, outline="blue")
+        win.canvas.create_line(xP2, yP2, xQ2, yQ2, fill="red")
+
+    #win.canvas.coords(win.roulette, xP2, yP2, xQ2, yQ2)
+    #win.canvas.coords(win.temoin, xP2, yP2, xQ2, yQ2)
+    #win.after(t, movator(d, t))
+
+
+
+
+
+
+
+
+
 def drawLine():
     #initialisation
     xa, ya, xb, yb = win.canvas.coords(win.item[-1])#coords du dernier oval

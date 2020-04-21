bc459cad6de116a0432606bee1861d0015f144d8
Votre nom ou pseudo <axelplanteyveux@gmail.com>
2020-04-21 13:40:31 +0200

    Ajout des frames pr remplacer bouton + renoomage

--- interface_config_display.py
+++ interface_config_display.py
@@ -24,22 +24,24 @@ def main():
 
     #Frame Begin
     
-    frameprince= Frame(win, width=width, height=height, bg='black',flow='W')#frame principale 
-    BigBand = Frame(frameprince,bg='yellow', width=width, height=height,flow='S', grow=False)
-    Left_Band = Frame(frameprince,bg='white', width=width, height=height,flow='S', border=5, grow=False)
-    midframe= Frame(BigBand, bg='red', width=500, height=700,border=4,flow='W',grow=False)
-    botframe=Frame(BigBand,bg='blue',width=width,height=height,border=4,grow=False)
-    play_ctr=Frame(midframe,bg='green',width=100,height=700,border=4,grow=False)
-    canvas=Canvas(midframe,bg='purple',width=width, height=700,border=5,grow=False)
+    win.frameprince= Frame(win, width=width, height=height, bg='black',flow='W')#frame principale 
+    win.BigBand = Frame(win.frameprince,bg='yellow', width=width, height=height,flow='S', grow=False)
+    win.Left_Band = Frame(win.frameprince,bg='white', width=width, height=height,flow='S', border=5, grow=False)
+    win.midframe= Frame(win.BigBand, bg='red', width=500, height=700,border=4,flow='W',grow=False)
+    win.botframe=Frame(win.BigBand,bg='blue',width=width,height=height,border=4,grow=False)
+    win.play_ctr=Frame(win.midframe,bg='green',width=100,height=700,border=4,grow=False)
+    win.canvas=Canvas(win.midframe,bg='purple',width=1000, height=700,border=5,grow=False)
+    win.frBt_fixe= Frame(win.Left_Band,bg='gray',width=width,height=300,grow=True)
+    win.frBt_rond= Frame(win.Left_Band,bg='yellow',width=width,height=300,grow=True)
+    win.frBt_trocho= Frame(win.Left_Band,bg='red',width=width,height=300,grow=True)
 
     #Frame end
 
     #Button Begin
-
-    Button(play_ctr,text='Lets play')
-    Button(Left_Band,text='Choix de la forme fixe')
-    Button(Left_Band,text='Choix de la forme du rond')
-    Button(Left_Band,text='Choix de la forme fixe')
+    Button(win.play_ctr,text='Lets play')
+    Button(win.frBt_fixe,text='Choix de la forme fixe')
+    Button(win.frBt_rond,text='Choix du rond fixe')
+    Button(win.frBt_trocho,text='Paramétres trochoides')
 
     #Button End
     

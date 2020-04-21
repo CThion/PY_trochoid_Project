b8de0b46a5440285412ab74f0324ad0f39d6e091
Clément Thion <clement.thion@orange.fr>
2020-04-21 17:36:56 +0200

    replaced numeral values on height/width by variable width & height

--- interface_config_display.py
+++ interface_config_display.py
@@ -14,42 +14,43 @@ from math import *
 def main():
     """our graphic interface"""
     global win
-    width, height = 1000, 1000
+    width, height = 500, 500
     win = Win(title='trochoïde', grow=True, flow='SE', op=2)
-    win.item = []
-    
 #=======================================(parent: win)=======================================================
-    win.frameTop= Frame(win, bg='red', width=width, height=25, grow=False)  
-    win.frameprince= Frame(win, width=width, height=height, bg='black',flow='W')#frame principale 
+    #frame for general options (language...)
+    win.frameTop= Frame(win, bg='red', width=width, height=height//40, grow=False)  
+    #principal frame
+    win.frameprince= Frame(win, width=width, height=height, bg='black', flow='W')#frame principale 
 
 #=======================================(parent: win.frameprince)===============================================
-    win.BigBand = Frame(win.frameprince,bg='yellow', width=width, height=height,flow='S', grow=False)
-    win.Left_Band = Frame(win.frameprince,bg='white', width=width, height=height,flow='S', border=5, grow=False)
+    win.BigBand = Frame(win.frameprince, bg='yellow', width=width, height=height, flow='S', grow=False)
+    win.Left_Band = Frame(win.frameprince, bg='white', width=width, height=height, flow='S', border=5, grow=False)
 
 #=======================================(parent: win.BigBand)===================================================
-    win.midframe= Frame(win.BigBand, bg='red', width=500, height=700,border=4,flow='W',grow=False)
-    win.botframe=Frame(win.BigBand,bg='blue',width=width,height=height,border=4,grow=False)
+    win.midframe= Frame(win.BigBand, bg='red', width=width/2, height=0.7*height, border=4,flow='W', grow=False)
+    win.botframe=Frame(win.BigBand,bg='blue', width=width, height=height, border=4, grow=False)
 
 #=======================================(parent: win.midframe)==================================================
     #frame at right containing button for starting and drawing control while playing 
-    win.play_ctr=Frame(win.midframe,bg='green',width=100,height=700,border=4,grow=False)
+    win.play_ctr=Frame(win.midframe, bg='green', width=0.1*width, height=0.7*height, border=4, grow=False)
     #canvas to display the trocho
-    win.canvas=Canvas(win.midframe,bg='purple',width=1000, height=700,border=5,grow=False)
+    win.canvas=Canvas(win.midframe, bg='purple', width=width, height=0.7*height, border=5, grow=False)
 
 #=======================================(parent: win.Left_Band)===============================================
     #confining buttons into individul frame to better control their dimentions
   #-----------------  
-    win.frBt_fixe= Frame(win.Left_Band,bg='gray',width=width,height=300,grow=True)
+    win.frBt_fixe= Frame(win.Left_Band, bg='gray', width=width, height=0.3*height, grow=True)
     Button(win.frBt_fixe,text='Choix de la forme fixe')
   #-----------------
-    win.frBt_rond= Frame(win.Left_Band,bg='yellow',width=width,height=300,grow=True)
-    Button(win.frBt_rond,text='Choix du rond mobile')
+    win.frBt_rond= Frame(win.Left_Band, bg='yellow', width=width, height=0.3*height, grow=True)
+    Button(win.frBt_rond, text='Choix du rond mobile')
   #-----------------
-    win.frBt_trocho= Frame(win.Left_Band,bg='red',width=width,height=300,grow=True)
+    win.frBt_trocho= Frame(win.Left_Band, bg='red', width=width, height=0.3*height, grow=True)
     Button(win.frBt_trocho,text='Paramétres trochoides')
   #-----------------
-
-Button(win.play_ctr,text='Lets play')
+    Button(win.play_ctr,text='Lets play')
+#=======================================()===============================================
+    win.loop()
 
    
 if __name__ == '__main__':

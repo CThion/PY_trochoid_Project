a4c47e4fc299484ada966724309f144167b1882f
Clément Thion <clement.thion@orange.fr>
2020-04-26 22:23:06 +0200

    formatage width-height-grow

--- trocho_game.py
+++ trocho_game.py
@@ -7,6 +7,7 @@ __date__    = "2020-04-"
 # ==============================================================================
 from ezTK import *
 from math import *
+import bib_drawer as bd #if import all, conflict with main() in bib_drawer. Eventually changed.
 # ==============================================================================
 
 def main_bot_band(key_butt):
@@ -49,52 +50,50 @@ def bot_band_trocho():
     
 #==============================================================================  
     
+
+
+
+
+
 def main():
-    """our graphic interface"""
+    """our graphic interface. La repartition du code dans main() suit la la logique de haut en bas gauche à droit de l'interface
+    """
     global win
-    width, height = 1200, 1000
-    win = Win(title='trochoïde', grow=True, flow='SE', op=2)
-#=======================================(parent: win)=======================================================
-    #frame for general options (language...)
-    win.top_band= Frame(win, bg='red', width=width, height=height//40, grow=False)  
-    #principal frame
-    win.frameking= Frame(win, width=1200, height=1000, bg='black', flow='W')#frame principale 
-
-#=======================================(parent: win.frameking)===============================================
-    win.big_band = Frame(win.frameking, bg='yellow', width=900, height=1000,op=0, flow='S', grow=True) #La largeur est égale à 1200 en sommant les deux largeurs
-    win.left_band = Frame(win.frameking, bg='white', width=300, height=1000,op=0, flow='S', grow=True)#On garde la même hauteur
-
-#=======================================(parent: win.big_band)===================================================
-    win.prince_band= Frame(win.big_band, bg='red', width=900, height=700,op=0,flow='W', grow=True) #On garde la largeur de la frame parent
-    #win.bot_band=Frame(win.big_band,bg='blue', width=900, height=300, border=4, grow=True) #Somme des hauteurs = 1000 
-      #welcome message
-    win.welcomeDisplay = Label(win.big_band, text='On mange des bébé mort.')
-#=======================================(parent: win.prince_band)==================================================
-    #frame at right containing button for starting and drawing control while playing 
-    win.right_band=Frame(win.prince_band, bg='cyan', width=100, height=700, grow=True)
-    #canvas to display the trocho
-    win.canvas=Canvas(win.prince_band, bg='white', width=800, height=700, grow=True)
-
-#=======================================(parent: win.left_band)=============================================== 
-    #confining buttons into individul frame to better control their dimentions
-  #-----------------  
-    win.frButt_fixe= Frame(win.left_band, bg='gray', width=300, height=333, grow=True)
-    Button(win.frButt_fixe, text='Choix de la forme fixe', command=lambda: main_bot_band(1))
-  #-----------------
-    win.frButt_rond= Frame(win.left_band, bg='cyan', width=300, height=333, grow=True)
-    Button(win.frButt_rond, text='Choix du rond mobile', command=lambda: main_bot_band(2))
-  #-----------------
-    win.frButt_trocho= Frame(win.left_band, bg='gray', width=300, height=333, grow=True)
-    Button(win.frButt_trocho, text='Paramétres trochoides', command=lambda: main_bot_band(3))
-  #-----------------
     
-    #would be a list to stock left_band's widget. If done, wouldn't be necessary to set <win.> to windget's name anymore.
-    #win.left_band_list = []
-    #win.left_band_list.append(win.frButt_fixe, win.frButt_rond, win.frButt_trocho)
-#=======================================(prent: win.play_ctr)===============================================
-    #Button(win.play_ctr,text='Lets play')
-#
-# 
+    width, height = 1300, 1000
+    win = Win(title='trochoïde', width=width, height=height, flow='SE')
+    win.width = win.winfo_width()
+    win.height = win.winfo_height()
+#=======================================(parent: win)=======================================================
+    win.top_band= Frame(win, bg='red', height=win.height//40)  
+  #----------------
+    win.frameking= Frame(win, bg='black', flow='E') 
+
+#=======================================( left_band )==parent:frameking===============================================
+    win.left_band = Frame(win.frameking, bg='white', width=0.2*win.winfo_width(), flow='S')
+#---------------------------------------------------------------------------------------------------------------------------
+    Button(win.left_band, text='Choix de la forme fixe', command=lambda: main_bot_band(1))
+  #-----------------
+    Button(win.left_band, text='Choix du rond mobile', command=lambda: main_bot_band(2))
+  #-----------------
+    Button(win.left_band, text='Paramétres trochoides', command=lambda: main_bot_band(3))
+  #-----------------
+
+#=======================================( big_band )==parent:frameking===============================================
+    win.big_band = Frame(win.frameking, bg='yellow', flow='S') #La largeur est égale à 1200 en sommant les deux largeurs
+#---------------------------(prince_band)
+    win.prince_band = Frame(win.big_band, bg='red', flow='W') #On garde la largeur de la frame parent
+#--------(right_band)   
+    win.right_band = Frame(win.prince_band, bg='cyan', width=100)
+#--------(canvas)
+    win.canvas = Canvas(win.prince_band, bg='white')
+#---------------------------(bot_band)
+    win.bot_band = Frame(win.big_band, bg='blue', height=height//10)
+    win.welcomeDisplay = Label(win.bot_band, text='On mange des bébé mort.')
+
+#=======================================(parent: win.prince_band)==================================================
+
+
     win.loop()
 
    
